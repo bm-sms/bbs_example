6c151ad1f804a49233deb91e7e1086e1076032eb
kajisha <kajisha@gmail.com>
2016-09-02 17:05:31 +0900

    rails g devise Bbs::User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, class_name: "Bbs::User"
   root to: 'welcome#index'
 
   mount Bbs::Engine => '/bbs'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902080020) do
+ActiveRecord::Schema.define(version: 20160902080146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,14 +48,9 @@ ActiveRecord::Schema.define(version: 20160902080020) do
   end
 
   create_table "bbs_users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                               null: false
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -65,8 +60,15 @@ ActiveRecord::Schema.define(version: 20160902080020) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["email"], name: "index_bbs_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_bbs_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "email",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "bbs_comments", "bbs_topics", column: "topic_id"
