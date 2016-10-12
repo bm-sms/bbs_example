af0d98fe84bd549385e19d5273ff0f55332f5927
kajisha <kajisha@gmail.com>
2016-10-12 17:14:34 +0900

    add users/registrations#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  devise_for :users, class_name: 'Bbs::User'
+  devise_for :users, class_name: 'Bbs::User', controllers: {
+    :registrations => 'users/registrations'
+  }
   ActiveAdmin.routes(self)
 
   root to: 'welcome#index'
