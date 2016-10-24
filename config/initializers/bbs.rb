ab0c500c7948b2ad5e8521c97c0f0b181de273aa
kajisha <kajisha@gmail.com>
2016-10-24 21:43:26 +0900

    rails g bbs:config

--- config/initializers/bbs.rb
+++ config/initializers/bbs.rb
@@ -1,5 +1,11 @@
 Bbs.configure do |config|
+  # clamp line specified words.
+  # for example, if you set `config.clamp_article_body` to `10`,
+  # comment or topics body will clamp 10 words when user does not logged in.
+  #
+  # If you set value `false`, never clamp line.
   config.clamp_article_body = 10
 
+  # spedify sign in path
   config.login_path = '/users/sign_in'
 end
