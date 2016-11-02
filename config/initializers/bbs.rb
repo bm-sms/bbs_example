99e69d985b53936f50a9a007cfc8d48a2359a2f5
kajisha <kajisha@gmail.com>
2016-11-02 16:39:22 +0900

    install bbs:config

--- config/initializers/bbs.rb
+++ config/initializers/bbs.rb
@@ -4,8 +4,23 @@ Bbs.configure do |config|
   # comment or topics body will clamp 10 words when user does not logged in.
   #
   # If you set value `false`, never clamp line.
-  config.clamp_article_body = 10
+  #config.clamp_article_body = 10
 
   # spedify sign in path
-  config.login_path = '/users/sign_in'
+  #config.login_path = '/users/sign_in'
+
+  # set number of latest topics inside component
+  #config.latest_topics_count = 10
+
+  # set number of topics per page
+  #config.topics_per_page = 10
+
+  # set number of topics per page
+  #config.comments_per_page = 10
+
+  # set current_user method
+  config.current_user = :current_user
+
+  # set authenticate user method
+  config.authenticate_user = :authenticate_user!
 end
